d81c64e96d640e61be432de50ceb6fb80b3f1f8c
rolando95 <rarj1795@outlook.com>
2019-06-22 19:34:11 -0500

    port to aligned arrays
    La memoria para tipos de datos como Matrix están segmentadas por filas actualmente. Por otra parte, los smartpointers, los accesos a elementos de Matrix funcionan de manera mejorable.
    
    Este port arreglará estos problemas y a la vez generalizará la estructura array para n cantidad de dimensiones de forma similar a como hace el array de Numpy (Python).
    
    De ahora en adelante, la estructura Vector y Matrix será únicamente Array y ésta independientemente del número de dimensiones que contenga, trabajará siempre todos los items en memoria alineados para mejorar la compatibilidad con otras librerías propias de C/C++.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
-#include "number.h"
-#include <string>
-#include <iostream>
-#include <thread>
-#include <windows.h>
-using namespace std;
 
-Number f(Number x){
-    return x*x+2;
-}
 int main(){
-    Vector a = range(10,0,-2);
-    print(a);
     return 0;
 }
