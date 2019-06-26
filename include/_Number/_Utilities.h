ffaf9187f7c527f24ece17e4e5548d3deb92fe7a
rolando95 <rarj1795@outlook.com>
2019-06-26 13:09:29 -0500

    La implementacion de range fue movida a un archivo cpp

--- include/_Number/_Utilities.h
+++ include/_Number/_Utilities.h
@@ -1,6 +1,6 @@
 #include "_Array.h"
 
-template<class T>
+template<class T=int>
 struct range{
     private:
         T _begin;
@@ -8,64 +8,9 @@ struct range{
         T _step;
         bool _isInverse=false;
     public:
-        range(): _begin(0), _end(0), _step(1) {}
-
-        range(T end){
-            _end = end;
-            if(end<0){
-                _isInverse = true;
-                _step = -1;
-            }
-        }
-        range(T begin, T end){
-            _begin = begin;
-            _end = end;
-            _step = 1;
-            if(begin>end){
-                _isInverse = true;
-                _step = -1;
-            }
-        }
-        range(T begin, T end, T step){
-            _begin = begin;
-            _end = end;
-            _step = step;
-            assert(
-                (begin<end && step>0) ||
-                (begin>end && step<0)
-            );
-        }
-        operator Array<T>(){
-            int count=0;
-            {
-                T j=_begin;
-                if(!_isInverse){
-                    while(j<_end){
-                        ++count;
-                        j+=_step;
-                    }
-                }else{
-                    while(j>_end){
-                        ++count;
-                        j+=_step;
-                    }
-                }
-            }
-            
-            Array<T> result;
-            result.resize(count);
-
-            {
-                int idx=0;
-                T j=_begin;
-                while(idx<count){
-                    result(idx) = j;
-                    j+=_step;
-                    ++idx;
-                }
-            }
-            
-            return result;
-        }
-
+        range();
+        range(T end);
+        range(T begin, T end);
+        range(T begin, T end, T step);
+        operator Array<T>();
 };
--- src/_Utilities.cpp
+++ src/_Utilities.cpp
@@ -1,36 +1,70 @@
-// #include "../include/_Number/_Utilities.h"
+#include "../include/_Number/_Utilities.h"
 
-// //Range class
-// template class range<int>;
-// template class range<float>;
-// template class range<double>;
+//Range class
+template class range<int>;
+template class range<float>;
+template class range<double>;
 
-// template<class T> range<T>::range(): _begin(0), _end(0), _step(1) {}
+template<class T> range<T>::range(): _begin(0), _end(0), _step(1) {}
 
-// template<class T> range<T>::range(T end){
-//     _end = end;
-//     if(end<0){
-//         _isInverse = true;
-//         _step = -1;
-//     }
-// }
+template<class T> range<T>::range(T end){
+    _end = end;
+    if(end<0){
+        _isInverse = true;
+        _step = -1;
+    }
+}
 
-// template<class T> range<T>::range(T begin, T end){
-//     _begin = begin;
-//     _end = end;
-//     if(begin>end){
-//         _isInverse = true;
-//         _step = -1;
-//     }
-// }
+template<class T> range<T>::range(T begin, T end){
+    _begin = begin;
+    _end = end;
+    _step = 1;
+    if(begin>end){
+        _isInverse = true;
+        _step = -1;
+    }
+}
 
-// template<class T> range<T>::range(T begin, T end, T step){
-//     _begin = begin;
-//     _end = end;
-//     _step = step;
-//     assert(
-//         (begin>end && step>0) ||
-//         (begin<end && step<0)
-//     );
-// }
+template<class T> range<T>::range(T begin, T end, T step){
+     _begin = begin;
+    _end = end;
+    _step = step;
+    assert(
+        (begin<end && step>0) ||
+        (begin>end && step<0)
+    );
+}
+
+template<class T> range<T>::operator Array<T>(){
+    int count=0;
+    {
+        T j=_begin;
+        if(!_isInverse){
+            while(j<_end){
+                ++count;
+                j+=_step;
+            }
+        }else{
+            while(j>_end){
+                ++count;
+                j+=_step;
+            }
+        }
+    }
+    
+    Array<T> result;
+    result.resize(count);
+
+    {
+        int idx=0;
+        T j=_begin;
+        while(idx<count){
+            result(idx) = j;
+            j+=_step;
+            ++idx;
+        }
+    }
+    
+    return result;
+}
 
