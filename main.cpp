b576aefe55add8f50aa03550590c84a6c30ce154
rolando95 <rarj1795@outlook.com>
2019-09-18 09:40:16 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,8 @@ int main(){
 
     NDArray<string> hello  = items<string>("Hello", "World");
     NDArray<number> myList = range<number>(10);
-
-    hello(12) = 15;
-
     print(hello, myList);
-    Tree<int> a;
+
     input();
     return 0;
 }
