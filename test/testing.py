807d71b5abace631a6b94f74747f2706c792e8e4
rolando95 <rarj1795@outlook.com>
2019-11-12 20:07:33 -0500

    Local testing

--- test/testing.py
+++ test/testing.py
@@ -11,12 +11,13 @@ print("\n########### NO PARALLEL ###########\n")
 for file in testFiles:
     print("\n----- "+file+" -----\n")
     if(subprocess.call(["g++", file, "-o", "test", "-ggdb3", "-std=c++11", "-Wno-literal-suffix"])): exit(1)  
-    print("\n\n")
-    if(subprocess.call(["valgrind", "--leak-check=full", "--show-leak-kinds=all", "--error-exitcode=1", "./test"])): exit(1)
+    if(len(sys.argv)>1 and sys.argv[1]=="usevalgrind"):
+        if(subprocess.call(["valgrind", "--leak-check=full", "--show-leak-kinds=all", "--error-exitcode=1", "./test"])): exit(1)
+    else:
+        if(subprocess.call(["./test"])): exit(1)
 
 print("\n########### PARALLEL ###########\n")
 for file in testFiles:
     print("\n----- "+file+" -----\n")
     if(subprocess.call(["g++", file, "-o", "test", "-ggdb3", "-std=c++11", "-Wno-literal-suffix", "-fopenmp", "-D", "DM4thParallel"])): exit(1)  
-    print("\n\n")
     if(subprocess.call(["./test"])): exit(1)
