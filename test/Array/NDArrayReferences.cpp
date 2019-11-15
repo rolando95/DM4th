68157ebcb7db73f828cb956130b7cb486292578c
rolando95 <rarj1795@outlook.com>
2019-11-15 15:52:45 -0500

    Más operaciones in-place de prueba para NDArray

--- test/Array/NDArrayReferences.cpp
+++ test/Array/NDArrayReferences.cpp
@@ -1,12 +1,14 @@
 #include "../../DM4th/DM4th.h"
 #include "../DM4thTest.h"
 
+// Assign value into ref array
 NDArray<number> dummyFunction(NDArray<number> array, int idx, number value)
 {
     array(idx) = value;
     return array;
 }
 
+// Doesn't doing nothing
 NDArray<number> dummyFunction2(NDArray<number> array)
 {
     array = range<number>(10);
@@ -20,19 +22,31 @@ int main()
         NDArray<number> A, B, C;
         A = range<number>(10);
 
+        // Ref assignments
         C = B = dummyFunction(A, 0, -50i);
 
         C(5) = 150;
         B(2) = 35i;
 
+        // In-place operations
         dummyFunction2(A);
+        A += B;
+        A -= B;
+        A += 1;
+        A -= 1;
+        A *= 2;
+        A /= 2;
+        A.resize(3,5);
+        A.reshape(15);
+        A *= items<number>(2);
 
         EXPECT_EQ(A.refCount(), 3);
-        EXPECT_FALSE(A.isEqualTo(NDArray<number>(range<number>(10))));
         EXPECT_TRUE( (A==B).all() );
         EXPECT_TRUE(A.isEqualTo(C));
+        EXPECT_TRUE( A.isSameRef(B) );
+        EXPECT_FALSE( !B.isSameRef(C) );
         
-        
+        // Copy assignments
         B = A.getCopy();
         C = B.getCopy();
 
@@ -47,6 +61,7 @@ int main()
 
         EXPECT_FALSE(A.isEqualTo(C));
         EXPECT_FALSE(A.isEqualTo(B));
+
     });
 
 
