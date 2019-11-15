fe2e1b6d22a215d633c97577e368bb76fbb3868a
rolando95 <rarj1795@outlook.com>
2019-11-15 10:36:00 -0500

    Update testing.py

--- test/testing.py
+++ test/testing.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 import glob
-print("Uhmm")
+
 testFiles=glob.glob("**/*.cpp", recursive=True)
 
 print("Test Files:", testFiles)
@@ -12,7 +12,7 @@ for file in testFiles:
     print("\n----- "+file+" -----\n")
     if(subprocess.call(["g++", file, "-o", "test", "-ggdb3", "-std=c++11", "-Wno-literal-suffix"])): exit(1)  
     if(len(sys.argv)>1 and sys.argv[1]=="usevalgrind"):
-        if(subprocess.call(["valgrind", "--leak-check=full", "--show-leak-kinds=all", "--error-exitcode=1", "./test"])): exit(1)
+        if(subprocess.call(["valgrind", "--leak-check=full", "--show-leak-kinds=all", "--error-exitcode=1", "-v", "./test"])): exit(1)
     else:
         if(subprocess.call(["./test"])): exit(1)
 
