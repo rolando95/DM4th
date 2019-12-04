14c0cb1666e8a5ddb1cae6cbcc3a0a8f4915df39
rolando95 <rarj1795@outlook.com>
2019-12-03 22:18:18 -0500

    Ajuste en funciones de alojamiento en BaseArray
    Los métodos que realizan operaciones de alojamiento en memoria en un NDArray lo hacen en potencia de 2. Si, por ejemplo, reescalas un arreglo de tamaño 18, internamente se reescala a 32.

--- DM4th/Array/BaseArray_impl.h
+++ DM4th/Array/BaseArray_impl.h
@@ -9,8 +9,21 @@ namespace DM4thInternal
 {
 
 //#define sizeAligned(x)   (x + 3) & -4
-#define sizeAligned(x) (x + 3) & ~0x03
+//#define sizeAligned(x) (x + 3) & ~0x03
 
+inline int sizeAligned(int n)
+{ 
+    //if(n<4) return 4;
+    
+    n--; 
+    n |= n >> 1; 
+    n |= n >> 2; 
+    n |= n >> 4; 
+    n |= n >> 8; 
+    n |= n >> 16; 
+    n++; 
+    return n; 
+}  
 //////////////////// BaseArray
 
 
